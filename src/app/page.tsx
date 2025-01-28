8832e277ed4eb665fa7530f2d1be29288ad1cead
gilbruno <gilles_bruno@gmx.fr>
2025-01-28 16:14:07 +0100

    add texts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
       <ImageHeader />
       <EventInfo />
       <ArtistProfile />
-      <WhyChooseUs/>
       <Expertise/>
       <CatalogueStats/>
+      <WhyChooseUs/>
       <Testimonials/>
       <TrustedPartners/>
       <CatalogueRequest/>
